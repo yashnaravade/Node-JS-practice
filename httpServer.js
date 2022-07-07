57943efa2c927981b7a23c12c3ac5880f09d015f
Yash Naravade <yashdip123@gmail.com>
2022-07-07 23:43:37 +0530

    HTTP server

---
+++ httpServer.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = process.env.port;
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Wassup, CM_GoGo here!');
+  
+});
+
+server.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
