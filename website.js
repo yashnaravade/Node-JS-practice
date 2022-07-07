c9eab235b74fa954a6f035b63766470c967f309e
Yash Naravade <yashdip123@gmail.com>
2022-07-08 01:32:00 +0530

    Redirecting to different pages based on url

--- website.js
+++ website.js
@@ -1,36 +1,33 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  console.log(req.url);
+
+  if (req.url === "/") {
+    res.statusCode = 200;
+    const data = fs.readFileSync("./index.html", "utf8");
+    res.end(data.toString());
+  } else if (req.url === "/api") {
+    res.statusCode = 200;
+
+    res.end("This is api");
+  } else if (req.url === "/index") {
     
-   
-    res.setHeader('Content-Type', 'text/html');
-    console.log(req.url);
-    if(req.url === '/'){
     res.statusCode = 200;
-
-        res.end('Wassup, CM_GoGo here!');
-    }
-    else if(req.url === '/api'){
+  } 
+  else if (req.url === "/about") {
     res.statusCode = 200;
-
-        res.end('This is api');
-    }
-    else if(req.url === '/index'){
-        const fs = require('fs');
-        const data = fs.readFileSync('./index.html', 'utf8');
-        res.statusCode = 200;
-        res.end(data.toString());
-
-    }
-    else{
-        res.statusCode = 404;
-        res.end('Not found');
-    }
- 
+    res.end("This is about");
+  }else {
+    res.statusCode = 404;
+    res.end("Not found");
+  }
 });
 
-server.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
