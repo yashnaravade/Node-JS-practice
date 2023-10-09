882c3e59207f0ae1952179d1c28516f14741b6a0
Yash <73131357+yashnaravade@users.noreply.github.com>
2023-10-09 06:46:24 +0000

    Playing with the new guy in the town, Bun

---
+++ bun/bun-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+
+  const element = <h1>Hello, world!</h1>;
+
+  return (
+    <div className="App">
+  <h2>Bun works</h2>
+  {element}
+    </div>
+  );
+}
+
+export default App;
